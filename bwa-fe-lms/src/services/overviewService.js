986e3342c0a02dd7cd5eb111240affa8dc3ae9e1
nabilfwz <151445754+nabilfwz@users.noreply.github.com>
2025-10-24 21:36:12 +0700

    Delete bwa-fe-lms directory

--- bwa-fe-lms/src/services/overviewService.js
+++
@@ -1,3 +0,0 @@
-import { apiInstanceAuth } from "../utils/axios";
-
-export const getOverviews=async()=>apiInstanceAuth.get('/overviews').then(res=>res.data)
--- bwa-fe-lms/src/utils/const.js
+++
@@ -1,3 +0,0 @@
-export const STORAGE_KEY='STORAGE_KEY'
-export const MANAGER_SESSION='manager-session'
-export const STUDENT_SESSION='student-session'
